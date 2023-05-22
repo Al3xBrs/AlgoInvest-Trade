5fb3732bb24ca3286b48f725685783b6ab6335e3
Alex Brs <alexbrise.40@gmail.com>
2023-05-22 11:32:59 +0200

    read csv file

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,13 @@
-print("hello wolrd")
+import csv
+
+DATA_FILE = "src/data.csv"
+
+def getData(DATA_FILE):
+    with open(DATA_FILE, "r") as f:
+        obj = csv.reader(f)
+
+        for ligne in f :
+            print(ligne)
+            
+
+getData(DATA_FILE)
