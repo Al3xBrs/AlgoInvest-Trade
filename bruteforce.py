d42f8d4ec13761fa26bb1dce691c6ccb918d77bb
Alex Brs <alexbrise.40@gmail.com>
2023-05-23 17:35:45 +0200

    add minor update

--- bruteforce.py
+++ bruteforce.py
@@ -44,26 +44,3 @@ print("nb combs tot = ", len(combinations))
 # for i in range 1, len(comb) + 1
 
 # Formule combinaison : C(n,r) = n! / (r!(n-r)!)
-
-
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        F = 1
-        for k in range(2, n + 1):
-            F = F * k
-
-        return F
-
-
-def combForm(n, r):
-    nbCombinations = factorial(n) / (factorial(r) * factorial(n - r))
-    return nbCombinations
-
-
-nbTotComb = 0
-for i in actions:
-    nbTotComb = nbTotComb + combForm(len(actions), i)
-
-print(nbTotComb)
