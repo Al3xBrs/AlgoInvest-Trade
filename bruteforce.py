91bd4d7724960ac32f4007fa4ceceb25f5e2f7ab
Alex Brs <alexbrise.40@gmail.com>
2023-05-23 11:57:53 +0200

    update getData function

--- bruteforce.py
+++ bruteforce.py
@@ -2,12 +2,16 @@ import csv
 
 DATA_FILE = "src/data.csv"
 
+
 def getData(DATA_FILE):
+    actionsList = []
     with open(DATA_FILE, "r") as f:
-        obj = csv.reader(f)
+        obj = csv.reader(f, delimiter=";")
 
-        for ligne in f :
-            print(ligne)
-            
+        for name, price, profit in obj:
+            actionsList.append({name: {"price": int(price), "profit": int(profit)}})
 
-getData(DATA_FILE)
+        return actionsList
+
+
+print(getData(DATA_FILE))
